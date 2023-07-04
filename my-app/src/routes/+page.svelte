0567aa978c0855c5d9ded12d92b7e26ffe17aaa6
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-04 14:38:59 +0700

    Create child and import props

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
+import Message from './Message.svelte';
+
+const msg = 'Hello from Parent';
+
 let name = 'Hawari Muflih Munte'
 </script>
 
 <p>Hello there, my name is {name}</p>
+<Message message={msg}></Message>
 
